65ba989ae057e1c5483326daba7202311336c432
lsamano <leizl.samano@flatironschool.com>
2019-10-19 18:35:16 -0400

    Whatever.

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Photo from './achievement.jpg';
 
 const Home = ({currentUser, history}) => {
   console.log(currentUser);
@@ -16,15 +16,15 @@ const Home = ({currentUser, history}) => {
     return (
       <div style={{textAlign:"center"}}>
       <h1 style={{paddingTop:"40px",height:"100px",backgroundColor:"#025373", color:"white"}}>
-      {`Welcome, ${currentUser.username}.`}
-      </h1>
-      <h3>Your group: {group.name}</h3>
-      <br/>
-      <h3>Your Upcoming Meetings:</h3>
-      <ul>
-      {meetings.map(meeting => <li>{meeting.dateTime}</li>)}
-      </ul>
-      <button onClick={clickHandler}>Enter Chat</button>
+      {`Welcome, ${currentUser.username}. to Virtu Narcotics Anonymous`}</h1>
+      <img src={Photo} style={{width:"100%"}} alt="People climbing a mountain"/>
+        <h3>Your group: {group.name}</h3>
+        <h3>Your Upcoming Meetings:</h3>
+        <ul>
+        {meetings.map(meeting => <p>{meeting.dateTime}</p>)}
+        </ul>
+        <button style={{margin:"20px", fontSize:"16px", color:"black", width:"100%", height:"40px", backgroundColor:"#83A8BF", border:"0"}} onClick={clickHandler}>Enter Chat</button>
+
       </div>
     );
   }
