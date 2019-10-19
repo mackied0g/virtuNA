27ab58d248193664c8e65b0b5cb444c63d9012a9
Grace M. Rodriguez <gracem.rodriguez@Graces-MBP.router>
2019-10-19 17:03:53 -0400

    minor css changes.

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import ChatPage from "./ChatPage/ChatPage";
 function App() {
     return (
       <div className="App">
-          {/* <ChatPage/> */}
+          <ChatPage/>
       </div>
     );
   }
