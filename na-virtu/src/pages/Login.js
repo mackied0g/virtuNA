9e6e7e549161a38a18e35d04d9cc377e17aaec46
lsamano <leizl.samano@flatironschool.com>
2019-10-19 12:07:12 -0400

    added login button, no functionality yet

---
+++ na-virtu/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+
+  return (
+    <div>
+    Hello
+    <div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-auto-logout-link="true" data-use-continue-as="false"></div>
+    </div>
+  );
+
+}
+
+export default Login;
